c5c37553dc112976e6de7f98ad97af251654b647
wqmeng <216289+wqmeng@users.noreply.github.com>
2023-08-23 01:17:46 +0800

    correct the pvpgn1.99.8.0.0-rc1-PRO.7z uri.

--- src/build_wine.sh
+++ src/build_wine.sh
@@ -43,14 +43,13 @@ WINEPREFIX=~/.wine WINEARCH="win32" wine winecfg
 # 后面添加具体的服务器的版本文件.
 # wget -qO diablo2_bnet.7z "http://10.0.0.10/diablo2_bnet.7z"
 # 7za x -y diablo2_bnet.7z
-# wget -qO HGD2.7z "http://10.0.0.10/HGD2.7z"
-# 7za x -y HGD2.7z
+
 cd /home
 rm /home/pvpgn -rf
 rm /home/d2gs -rf
 # mkdir -p /home/pvpgn
 
-wget -q https://raw.githubusercontent.com/wqmeng/pvpgner/main/src/pvpgn1.99.8.0.0-rc1-PRO.7z
+wget -q https://github.com/wqmeng/pvpgner/raw/main/pvpgn/pvpgn1.99.8.0.0-rc1-PRO.7z
 7za x -y pvpgn1.99.8.0.0-rc1-PRO.7z
 mv pvpgn1.99.8.0.0-rc1-PRO pvpgn
 # rm pvpgn1.99.8.0.0-rc1-PRO -rf
